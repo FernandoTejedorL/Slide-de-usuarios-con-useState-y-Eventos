596a982e4af901a813de93ab34ff193f88dd9bd0
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2024-12-10 15:14:02 +0100

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
-import Button from './components/button/Button';
-import { GlobalStyles } from './styles/GlobalStyles';
+import { useState } from 'react';
+import { USERS } from './constants/users-info';
 
 const App = () => {
+	const [counter, setCounter] = useState(0);
+	const maximum = USERS.length - 1;
 	return (
 		<div>
-			<GlobalStyles />
-			<h1>Curso de React</h1>
-			<Button color='steelblue'>Click Me!</Button>
-			<Button color='#f0f'>Click Me!</Button>
+			<img src={USERS[counter].profileImage} alt='' />
+			<h1>Name: {USERS[counter].name}</h1>
+			<h2>Username: {USERS[counter].username}</h2>
+			<h3>Email: {USERS[counter].email}</h3>
+			<button
+				disabled={counter === 0}
+				onClick={() => previous(counter, setCounter)}
+			>
+				Previous
+			</button>
+			<button
+				disabled={counter === maximum}
+				onClick={() => next(counter, setCounter)}
+			>
+				Next
+			</button>
 		</div>
 	);
+};
+
+const next = (counter, setCounter) => {
+	setCounter(counter + 1);
+};
+
+const previous = (counter, setCounter) => {
+	setCounter(counter - 1);
 };
 
 export default App;
